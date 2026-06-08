3ac1a11715c3555b6f1b99254a47dc7902fb41e2
ta234567801@gmail.com <ta234567801@gmail.com>
2026-06-08 18:15:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// This is a Javascript File
+// Data Types
+
+// //  <---- comment ctrl + Question mark
+
+// 1. String "", '', ``
+// 2. Numbers 1232445, 3.14159
+// 3. Boolean True or False
+
+let name1 = "Talal Ahmed"
+let name2 = "Faheem Ahmed"
+
+console.log(name2, name1)
+
