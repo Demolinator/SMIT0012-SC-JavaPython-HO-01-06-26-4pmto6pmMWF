6689f17822fd1d1dd8c6c70b48f99ca11d2f297d
ta234567801@gmail.com <ta234567801@gmail.com>
2026-06-10 18:17:52 +0500

    Added: Javascript Class 04 added

---
+++ Javascript/Class_02_lab_5_6_2026/app.js
@@ -0,0 +1,28 @@
+// Variable
+
+let firstName;
+let lastName = "Ahmed";
+
+// Assign value to variable
+
+firstName = "Talal";
+
+// Print value of variable
+
+console.log(firstName, lastName);
+
+
+
+
+
+
+
+// Number
+
+let pi = 3.14159;
+let radius = 5;
+
+console.log(pi + " X " + radius);
+console.log(pi + radius);
+
+// concatination
---
+++ Javascript/Class_04_10_6_2026/app.js
@@ -0,0 +1,62 @@
+// Arthematic Operations
+// +, -, *, /, %, **
+// BODMAS - Bracket, Order, Division, Multiplication, Addition, Subtraction
+
+// let num1 = 10;
+// let num2 = 10;
+// // let sum = num1 + num2;
+
+// console.log("The sum is:" + (num1 + num2)); // console.log("The sum is:" + sum);
+// console.log("The subtraction is:" + (num1 - num2));
+// console.log("The multiplication is:" + num1 * num2);
+// console.log("The division is:" + num1 / num2);
+// console.log("The modulus is:" + num1 % num2);
+// console.log("The exponentiation is:" + num1 ** num2);
+
+
+
+// if (condition) {expression}
+
+
+// Comparison Operators >, <, >=, <=, ===, !==
+
+// if (5 > 3) {
+//     console.log("5 is greater than 3");
+// } else {
+//     console.log("5 is not greater than 3");
+// }
+
+
+// let age = 18
+
+// if (age > 19) {
+//     console.log("You are eligible for CNIC");
+// } else {
+//     console.log("You are not eligible for CNIC");
+// }
+
+// console.log(18 !== 19);
+
+
+// let shop = "Milk";
+
+// if (shop === "Dahi") {
+//     console.log("Bring Dahi");
+// } else {
+//     console.log("bring Milk");
+// }
+
+
+
+let shop = "Dahi";
+
+if (shop === "Biryani") {
+    console.log("Bring Biryani");
+} 
+else if (shop === "Pulao") {
+    console.log("Bring Pulao");
+} 
+else {
+    console.log("Bring Burger");
+}
+
