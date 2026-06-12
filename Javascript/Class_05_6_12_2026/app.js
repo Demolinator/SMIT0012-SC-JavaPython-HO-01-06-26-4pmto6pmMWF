0c23d7772499a46ab814dd74cbfb0ea4b895c475
ta234567801@gmail.com <ta234567801@gmail.com>
2026-06-12 17:55:49 +0500

    Added:CLass 05 Javascript and intro to HTML

---
+++ Javascript/Class_05_6_12_2026/app.js
@@ -0,0 +1,31 @@
+// && Logical AND operator
+
+// let age = 150;
+
+// if (age >= 18 && age <= 100) {
+//     console.log("You are eligible for CNIC.");
+// } 
+// else if (age >= 200) {
+//     console.log("you are not alive or you are a ghost.");
+// }
+// else if (age > 100) {
+//     console.log("You are given lifetime CNIC.");
+// }
+// else {
+//     console.log("You are not eligible for CNIC.");
+// }
+
+
+let name = "Subhan";
+let money = 1500;
+
+
+if (name === "Subhan" && money <= 1000) {
+    console.log("Mubarak yeh lo Ice Cream.");
+} 
+else if (name === "Subhan" && money <= 500) {
+    console.log("Mubarak yeh lo Chai.");
+} 
+else {
+    console.log("Bhago, tumhare paas paisa nahi hai.");
+}
