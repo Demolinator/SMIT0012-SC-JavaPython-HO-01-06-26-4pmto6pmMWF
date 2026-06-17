ea8000165d1eee195baef6cee4a9ceed9d00ebbd
ta234567801@gmail.com <ta234567801@gmail.com>
2026-06-17 17:56:36 +0500

    Added:Javascript class 07 added For loops

---
+++ Javascript/Class_07_6_17_2026/app.js
@@ -0,0 +1,72 @@
+// Case-Sensitive
+// ===, ==
+
+// let Name = "Talal";
+// let num = "5";
+// Logical Operator ======> AND = &&, OR = ||, NOT = !
+
+
+// let name = "Talal";
+
+// console.log(name === "talal" || name === "TalaL");
+
+
+
+// let milkShop = "Dahi";
+// let resturant = "Nihari";
+
+// if (milkShop === "dahi" && resturant === "Nihari") {
+//     console.log("Dahi kiyon nhi lae?")
+//     console.log("Dahi Kharab tha.")
+// }
+// else if (milkShop === "Dahi" && resturant === "Biryani") {
+//     console.log("Nihari kiyon nhi lae?")
+//     console.log("Nihari nhi thi Biryani thi.")
+// }
+// else if (milkShop === "dahi" && resturant === "Biryani") {
+//     console.log("Kuch bhi nhi lae?")
+//     console.log("Nihari nhi thi or Dahi kharab tha.")
+//     console.log("Chappal Khane ke liye tayyar hojao.")
+// }
+// else {
+//     console.log("Bach gae dono lee aae.")
+// }
+
+
+// console.log(!false);
+// console.log(!0);
+
+// Post-Increment i++, Pre-Increment ++i, Post-Decrement i--, Pre-decrement --i
+
+// Post-Increment ===> Baad men plus 1 kerdena
+// let i = 12;
+
+// console.log(i++, i)// +1
+// console.log(i++) // 1
+// console.log(i)
+// console.log(i)
+
+// Pre-Increment ===> Pehle Plus 1 kerdena
+// let i = 12;
+
+// console.log(++i)// +1
+// console.log(i) // 1
+// console.log(i)
+// console.log(i)
+
+// Post-Decrement 
+// let num = 12;
+// console.log(num--); -1
+// console.log(num--);
+// console.log(num);
+
+// Pre-Decrement 
+// let num = 12;
+// console.log(--num); // 11
+// console.log(num--, --num);// 11, 9
+// console.log(num);// 9
+
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i + " X " + i + " = " + i * i);
+}
