300f0960d512202e0fc8345eaf5cdefa1ffd07d7
Talal Ahmed <ta234567801@gmail.com>
2026-06-19 17:54:12 +0500

    Added: Javascript class 08 Added For loops practice

---
+++ Javascript/Class_08_6_19_2026/app.js
@@ -0,0 +1,50 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(1 + " " +"X"+ " " + i + " " +"="+ " " + i * 1);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(2 + " " +"X"+ " " + i + " " +"="+ " " + i * 2);
+// }
+
+// 20 ----> 1 Rocket Blast off!
+
+// for (let i =20; i > 0; i--) {
+//     console.log(i);
+// }
+
+// console.log("Rocket Blast off!")
+
+
+// Print your name five times.
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Talal")
+// }
+
+// print even numbers from 2 to 50
+// variable += 2
+
+// for (let i = 2; i <= 50; i += 2) {
+//     console.log(i)
+// }
+
+// for (let chocolates = 1; chocolates <= 10; chocolates++) {
+//     console.log("You got chocolate number" + chocolates)
+// }
+
+
+
+
+// Stars **********
+
+// *****
+// *****
+// *****
+
+
+let stars = ""; // empty string
+
+for (let i = 1; i <= 100; i++) {
+    stars += "*" 
+    console.log(stars)
+}
